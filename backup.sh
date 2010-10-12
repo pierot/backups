64d7d02fedd1ed468790e8fc785bd4de5ae6afdd
Pieter Michels <pieter.michels@gmail.com>
2010-10-12 10:13:08 +0200

    backup completed on

---
+++ backup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# SETUP
+cd ~./backups
+
+# MYSQL
+DEST = "./mysql"
+
+rsync -av /usr/local/var/mysql/ $DEST
+
+# APACHE
+DEST = "./apache"
+
+rsync -av /private/etc/hosts $DEST
+rsync -av /private/etc/apache2/extra/httpd-vhosts.conf $DEST
+rsync -av /private/etc/apache2/httpd.conf $DEST
+
+# SAVE
+DATE = date
+
+git add .
+git commit -a -m "backup completed on $DATE"
+git push origin master
