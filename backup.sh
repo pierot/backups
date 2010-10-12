16119201c597fe9838c646547670882e66019746
Pieter Michels <pieter.michels@gmail.com>
2010-10-12 14:39:18 +0200

    export path

--- backup.sh
+++ backup.sh
@@ -17,7 +17,7 @@ gem list > ./info/gem.txt
 rvm list > ./info/rvm.txt
 
 # SYNC TO DROPBOX
-rsync -a ./apache ./mysql ~/Documents/Dropbox/Private/macprox/devenv/
+rsync -a ./apache ./mysql ~/Documents/Dropbox/Private/macprox/settings/devenv/
 
 # SAVE
 #git add .
