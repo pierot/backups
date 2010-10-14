1e23e303f5033bee0c877546d5beb55186d9f33b
Pieter Michels <pieter.michels@gmail.com>
2010-10-14 08:25:15 +0200

    backup completed

--- backup.sh
+++ backup.sh
@@ -3,6 +3,9 @@
 # SETUP
 cd ~/.backups
 
+# LAUNCHAGENT FILE
+rsync -a /Users/pieterm/Library/LaunchAgents/be.wellconsidered.backups ./
+
 # MONGODB
 rsync -a /usr/local/var/mongodb/ ./mongodb
 
@@ -15,14 +18,14 @@ rsync -a /private/etc/apache2/extra/httpd-vhosts.conf ./apache
 rsync -a /private/etc/apache2/httpd.conf ./apache
 
 # INFO
-brew list > ./info/brew.txt
-gem list > ./info/gem.txt
-rvm list > ./info/rvm.txt
+which brew | awk '{print $0}' | while read f; do "$f" list > ./info/brew.txt; done
+which gem | awk '{print $0}' | while read f; do "$f" list > ./info/gem.txt; done
+which rvm | awk '{print $0}' | while read f; do "$f" list > ./info/rvm.txt; done
 
 # SYNC TO DROPBOX
 rsync -a ./apache ./mysql ./mongodb ~/Documents/Dropbox/Private/macprox/settings/devenv/
 
 # SAVE
-#git add .
-#git commit -a -m "backup completed"
-#git push origin master
+git add .
+git commit -a -m "backup completed"
+git push origin master
